11853c45da9a7d1a05de79ecfa52c1b031df06cc
lihbr <lihbr@users.noreply.github.com>
2023-10-02 14:52:54 +0200

    Initial commit

---
+++ slices/RichText/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Content } from '@prismicio/client'
+
+// The array passed to \`getSliceComponentProps\` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(getSliceComponentProps<Content.RichTextSlice>(
+  ['slice', 'index', 'slices', 'context']
+));
+</script>
+
+<template>
+  <section>
+    <PrismicRichText
+      :field="slice.primary.content"
+      class="richtext"
+    />
+  </section>
+</template>
+
+<style scoped>
+section:deep(.richtext) {
+  max-width: 600px;
+  margin: 6em auto;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+
+section:deep(.richtext .codespan) {
+  font-family: monospace;
+}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './slices/**/*.{js,ts,vue}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Nunito Sans Variable', 'sans-serif'],
+        heading: ['Nunito Variable', 'sans-serif'],
+      }
+    }
+  }
+}
