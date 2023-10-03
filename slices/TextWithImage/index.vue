dd0ff63f90218051e36686849f43aaeaba3d9345
lihbr <lihbr@users.noreply.github.com>
2023-10-03 13:03:16 +0200

    feat: text with image slice

---
+++ slices/TextWithImage/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Content } from "@prismicio/client";
+
+// The array passed to `getSliceComponentProps` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(
+  getSliceComponentProps<Content.TextWithImageSlice>([
+    "slice",
+    "index",
+    "slices",
+    "context",
+  ])
+);
+</script>
+
+<template>
+  <Bounded
+    :data-slice-type="slice.slice_type"
+    :data-slice-variation="slice.variation"
+  >
+    <div
+      class="grid gap-8 md:grid-cols-2 place-items-center"
+    >
+      <PrismicImage
+        :field="slice.primary.image"
+        class="rounded-lg"
+        :class="{
+          'md:order-2': slice.variation === 'imageRight'
+        }"
+      />
+      <div>
+        <PrismicText
+          :field="slice.primary.heading"
+          class="mb-2 heading heading--lg"
+          wrapper="h2"
+        />
+        <PrismicRichText
+          :field="slice.primary.body"
+          class="text-2xl font-body text-slate-600 max-w-md leading-10"
+        />
+      </div>
+    </div>
+  </Bounded>
+</template>
--- slices/index.ts
+++ slices/index.ts
@@ -8,4 +8,7 @@ export const components = defineSliceZoneComponents({
   hero: defineAsyncComponent(() => import("./Hero/index.vue")),
   rich_text: defineAsyncComponent(() => import("./RichText/index.vue")),
   testimonials: defineAsyncComponent(() => import("./Testimonials/index.vue")),
+  text_with_image: defineAsyncComponent(
+    () => import("./TextWithImage/index.vue")
+  ),
 });
